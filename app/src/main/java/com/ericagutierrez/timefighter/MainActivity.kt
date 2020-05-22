874f92afaf9ff4531cd8850477cac19bd23a2bdf
Erica Gutierrez <egutierrez411@gmail.com>
2020-05-22 17:39:09 -0400

    connect to repo

---
+++ app/src/main/java/com/ericagutierrez/timefighter/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.ericagutierrez.timefighter
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    internal var score = 0
+    internal lateinit var tapMeButton: Button
+    internal lateinit var gameScoreTextView: TextView
+    internal lateinit var timeLeftTextView: TextView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        gameScoreTextView= findViewById(R.id.gameScoreTextView)
+        tapMeButton = findViewById(R.id.tapMeButton)
+        timeLeftTextView = findViewById(R.id.timeLeftTextView)
+
+    tapMeButton.setOnClickListener{ view ->
+        incrementScore()
+
+    }
+    }
+
+    private fun incrementScore() {
+        score += 1
+        var newScore = getString(R.string.yourScore, score)
+        gameScoreTextView.text = newScore
+    }
+}
