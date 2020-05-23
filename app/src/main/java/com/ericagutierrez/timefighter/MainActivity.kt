f3f07b9304b5134289176b9e67449fb5dd10a8e9
Erica Gutierrez <egutierrez411@gmail.com>
2020-05-22 20:08:46 -0400

    adding countDown

--- app/src/main/java/com/ericagutierrez/timefighter/MainActivity.kt
+++ app/src/main/java/com/ericagutierrez/timefighter/MainActivity.kt
@@ -2,12 +2,17 @@ package com.ericagutierrez.timefighter
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
 import android.widget.Button
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     internal var score = 0
+    internal var gameStarted = false
+    internal lateinit var countDownTimer: CountDownTimer
     internal lateinit var tapMeButton: Button
+    internal var initialCountDown: Long = 60000
+    internal var countDownInterval: Long = 1000
     internal lateinit var gameScoreTextView: TextView
     internal lateinit var timeLeftTextView: TextView
 
@@ -24,6 +29,11 @@ class MainActivity : AppCompatActivity() {
     }
 
         gameScoreTextView.text = getString(R.string.yourScore, score)
+    }
+
+    private fun resetGame() {
+        score = 0
+        gameScoreTextView.text = getString(R.string.yourScore,
     }
 
     private fun incrementScore() {
